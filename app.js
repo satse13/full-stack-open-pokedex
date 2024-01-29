1089066ac8fbca22a2ff569339e8b7a1a72ac688
atobi <atobiasangeles@gmail.com>
2024-01-29 18:50:22 +0100

    Add dotenv package and configure environment variables

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 // get the port from env variable
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
